321ba7d9807b9250eb2c5ed9259f38c8a979581d
hazracodegit <dudekulahazrabee@gmail.com>
2025-05-15 22:33:24 +0530

    Update linear1.java

--- linear1.java
+++ linear1.java
@@ -1,5 +1,5 @@
 package DSA;
-// using recursion
+// linear search using recursion
 public class linear1 {
     public static void main(String[] args){
         int[] arr={10,20,30,40,50};
@@ -8,18 +8,14 @@ public class linear1 {
         int result=linearSearch(arr,target,index);
         if (result!=-1)
         System.out.println("element found at index: "+result);
-
-
-
     }
     public static int linearSearch(int arr[],int target,int index){
-        
         if (index >=arr.length){
         return -1;}
         if (arr[index]==target){
             return index;}
             else
             return linearSearch(arr, target,index+1);
-
     }
 }
+/*linear search using recursion is ineffective due to stack overflow (function calls) but having awareness how to implement linear search is important */
