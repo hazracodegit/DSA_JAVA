0ae0ab6385949c0952fd5fbfcb4bbe983a8cb8bc
Dudekula Hazra Bee <dudekulahazrabee@gmail.com>
2026-01-26 22:08:39 +0530

    Update BalancedParanthesis.java

--- StacksQueues./BalancedParanthesis.java
+++ StacksQueues./BalancedParanthesis.java
@@ -5,16 +5,23 @@
          *  Every closing bracket should have opening bracket
          *  Every opening bracket should have closing bracket
          *  Order of the brackets must be maintained
+  Why should we use Stack Data Structure to solve this Problem:
+         *  Because here the brackets are following the LIFO Order- i.e. the bracket which is recently opened or 
+            lastly opened is the first one to be to be closed. So Hence we are using Stack.
+         *  And the bracket which is first opened is the one which is closed at last.
   Example : String : (){()[]}
         Output : true
   Example : String : ([]{}([)]
         Output : false
-
-
-                           
-                           
-                           
-                           
+  Algorithm (In Steps) :
+        *   Push opening brackets ( { [ onto the stack
+        *   When a closing bracket appears:
+               ----> Stack must not be empty
+               ----> Top of stack must match the closing bracket
+               ----> If not → invalid
+        *   At the end:
+               ----> Stack must be empty → valid
+               ----> Not empty → Invalid
                            
 import java.util.Stack;
 
